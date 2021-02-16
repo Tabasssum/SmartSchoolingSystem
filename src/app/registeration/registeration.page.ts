3234d58d7c9db3b367687663f0c9a046e08b7777
umaishasan <umaishasan96@gmail.com>
2021-02-16 23:35:26 +0500

    first fyp

--- src/app/registeration/registeration.page.ts
+++ src/app/registeration/registeration.page.ts
@@ -1,32 +1,97 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Classes {
+  classId: number;
+  className: string;
+
+}
+
 @Component({
   selector: 'app-registeration',
   templateUrl: './registeration.page.html',
   styleUrls: ['./registeration.page.scss'],
 })
 export class RegisterationPage implements OnInit {
-  name:string;
-  email:string;
-  phone:number;
-  selctCate:string;
-  pass:string;
-  cpass:string;
+  selctClass: string;
+  selctUsr: string;
+  name: string;
+  id: number;
+  age: number;
+  fname: string;
+  fid: number;
+  email: string;
+  phone: number;
+  pass: string;
+  cpass: string;
+  forAll: boolean = false;
+  forStu: boolean = false;
+  class: Classes[] = [
+    {
+      classId: 1,
+      className: "class-1"
+    },
+    {
+      classId: 2,
+      className: "class-2"
+    },
+    {
+      classId: 3,
+      className: "class-3"
+    },
+    {
+      classId: 4,
+      className: "class-4"
+    },
+    {
+      classId: 5,
+      className: "class-5"
+    },
+    {
+      classId: 6,
+      className: "class-6"
+    },
+    {
+      classId: 7,
+      className: "class-7"
+    },
+    {
+      classId: 8,
+      className: "class-8"
+    },
+    {
+      classId: 9,
+      className: "class-9"
+    },
+    {
+      classId: 10,
+      className: "class-10"
+    }
+  ];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  userSelect() {
+    if (this.selctUsr === 'student') {
+      this.forStu = true;
+      this.forAll = false;
+    }
+    else {
+      this.forAll = true;
+      this.forStu = false;
+    }
   }
 
-  confirmPass(x){
-    var c = document.getElementById('cnfrm').style.backgroundColor = 'green';
-    var d = document.getElementById('cnfrm').style.backgroundColor = 'red';
-    if(this.cpass !== this.pass){
-      x.value = d;
-    }
-    else{
-      x.value = c;
-    }
-  }
+  // confirmPass(x){
+  //   var c = document.getElementById('cnfrm').style.backgroundColor = 'green';
+  //   var d = document.getElementById('cnfrm').style.backgroundColor = 'red';
+  //   if(this.cpass !== this.pass){
+  //     x.value = d;
+  //   }
+  //   else{
+  //     x.value = c;
+  //   }
+  // }
 
 }
--- src/app/scanner-page/scanner-page.page.ts
+++ src/app/scanner-page/scanner-page.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-scanner-page',
@@ -8,14 +10,23 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 })
 export class ScannerPagePage implements OnInit {
   scanned = null;
-
-  constructor(private barcode:BarcodeScanner) { }
+  studentPage = [
+    { title: 'Home Works', url: '', icon: 'book' },
+    { title: 'Quiz', url: '', icon: 'document' },
+    { title: 'Result', url: '', icon: 'medal' }
+  ];
+  constructor(private barcode:BarcodeScanner,public router:Router,public appComp:AppComponent) { }
 
   ngOnInit() {}
 
   scanCode(){
     this.barcode.scan().then(code => {
       this.scanned = code.text;
+      if(this.scanned === '1234'){
+        this.router.navigateByUrl('/student');
+        this.appComp.appPages = this.studentPage;
+        this.appComp.username = this.scanned;
+      }
     })
   }
 
